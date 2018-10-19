ec7c702c7737fcf836186a604c5e37cf28be77fb
s <s>
2018-10-19 10:45:12 +0800

    create vc12 mfc project in vs2013

---
+++ vc12/FileUtilityView.cpp
@@ -0,0 +1,96 @@
+
+// FileUtilityView.cpp : CFileUtilityView 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "FileUtility.h"
+#endif
+
+#include "FileUtilityDoc.h"
+#include "FileUtilityView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CFileUtilityView
+
+IMPLEMENT_DYNCREATE(CFileUtilityView, CView)
+
+BEGIN_MESSAGE_MAP(CFileUtilityView, CView)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CFileUtilityView 构造/析构
+
+CFileUtilityView::CFileUtilityView()
+{
+	// TODO:  在此处添加构造代码
+
+}
+
+CFileUtilityView::~CFileUtilityView()
+{
+}
+
+BOOL CFileUtilityView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO:  在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CFileUtilityView 绘制
+
+void CFileUtilityView::OnDraw(CDC* /*pDC*/)
+{
+	CFileUtilityDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO:  在此处为本机数据添加绘制代码
+}
+
+void CFileUtilityView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CFileUtilityView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CFileUtilityView 诊断
+
+#ifdef _DEBUG
+void CFileUtilityView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CFileUtilityView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CFileUtilityDoc* CFileUtilityView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CFileUtilityDoc)));
+	return (CFileUtilityDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CFileUtilityView 消息处理程序
